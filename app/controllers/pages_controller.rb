5150a2cb76b9dd55d9fd87c0bae908f1526b08d3
Kamil Sarzyński <kamil.s.sarzynski@gmail.com>
2021-09-02 15:48:50 +0200

    add controllers

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class PagesControllers < ApplicationController
+  def index; end
+end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ReviewsController
+      def create
+        review = Review.new(review_params)
+
+        if review.save
+          render json: ReviewSerializer(review).serialized_json
+        else
+          render json: { error: review.error.messages }, status: 422
+        end
+      end
+
+      def destroy
+        review = Review.find(params[:id])
+
+        if review.destroy
+          head :no_content
+        else
+          render json: { error: review.error.messages }, status: 422
+        end
+      end
+
+      private
+
+      def review_params
+        params.require(:review).permit(:title, :description, :score, :airline_id)
+      end
+    end
+  end
+end
